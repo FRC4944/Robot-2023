3e3d98e50e31371ebb5948e40c7a1ef482e117a8
WyattArmstrongWA <WyattArmstrongWA@gmail.com>
2023-03-19 16:03:49 -0600

    auto 5 does not work, better but not fixed

--- src/main/java/frc/robot/autos/Auto5.java
+++ src/main/java/frc/robot/autos/Auto5.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class Auto5 extends SequentialCommandGroup {
     private final Swerve m_swerve;
 
-    public double rotation = Math.PI / 6;
+    public double rotation =  Math.PI / 6;
 
     public Auto5(Swerve swerve){
         this.m_swerve = swerve;
@@ -29,13 +29,14 @@ public class Auto5 extends SequentialCommandGroup {
             new WaitUntil(2500),
             new intakeOff(), 
             new HorizontalFirstVerticalCommand(RobotContainer.verticalElevator, RobotContainer.horizontalElevator, RobotContainer.wrist, 0.05, -0.05, 1.1, true),
-            new Drive_Back_Command(swerve, 1.6, 0, Constants.Swerve.AutoMaxspeed,1000, true),
+            new Drive_Back_Command(swerve, 3, 0, Constants.Swerve.AutoMaxspeed,1450, true),
             new WaitUntil(500),
-            new Drive_Back_Command(swerve, 0, rotation, Constants.Swerve.AutoMaxspeed, 2000, true),
-            new HorizontalFirstVerticalCommand(RobotContainer.verticalElevator, RobotContainer.horizontalElevator, RobotContainer.wrist, 0.05, 0.05, .85, true),
-            new Drive_Intake(swerve, RobotContainer.intake, .3, 0, Constants.Swerve.AutoMaxspeed, true),
+            new Drive_Back_Command(swerve, 0, rotation, Constants.Swerve.AutoMaxspeed, 700, true),
+            new Drive_Back_Command(swerve, 0.1, 0, Constants.Swerve.AutoMaxspeed, 200, true),
+            new HorizontalFirstVerticalCommand(RobotContainer.verticalElevator, RobotContainer.horizontalElevator, RobotContainer.wrist, 0.05, 0.05, 1, true),
+            new Drive_Intake(RobotContainer.intake, Constants.Swerve.AutoMaxspeed, true),
+           
             new HorizontalFirstVerticalCommand(RobotContainer.verticalElevator, RobotContainer.horizontalElevator, RobotContainer.wrist, 0.05, 0.05, .85, true),
             new WaitUntil(700)
-        );  
-    }
+        );    }
 }
